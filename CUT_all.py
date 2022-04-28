ebbc6240521e642369d730d49f1829d6e701e63a
WalterLu3 <s10130134@gmail.com>
2022-04-28 13:00:40 -0500

    cut method added

--- CUT_all.py
+++ CUT_all.py
@@ -164,30 +164,96 @@ def vertexSeparatorCut(model, where):
                     sumVertex += val[v,c]
                 
                 if sumVertex < val[u,c] - 0.000001:
-                    m._count = m._count + 1
+                    #m._count = m._count + 1
                     #print("hey")
                     model.cbLazy(gp.quicksum(model._vars[v,c]
                                              for v in S) >= model._vars[u,c])
                 
         model._vals = None
         #print(centerList)
+    elif where == GRB.Callback.MIPNODE and m._count <= 10 :
+        status = model.cbGet(GRB.Callback.MIPNODE_STATUS)
+
+        if status == GRB.OPTIMAL:
+            val = model.cbGetNodeRel(model._vars)
+ 
+            #val = model.cbGetSolution(model._vars)
+            
+            # sorted the district center
+            centerList = []
+            for i in vertex:
+                centerList.append((i,val[i,i])) 
+            centerList = sorted(centerList, reverse=True, key=getSecond)   
+            
+            # starting to add cuts
+            hasAdditionalCuts = False
+            
+            
+            for center in centerList:
+                
+                c = center[0]
+                value = center[1]
+                #only add cuts for center that has great Xcc value and  when no cut is added
+                if value < ((1/VNum) - 0.001) and (not hasAdditionalCuts):
+                   break
+          
+                #set graph weight
+                for v in vertex:
+                    GRAPH.edges[(str(v)+"@in" , str(v)+"@out")]['capacity'] = val[v,c]
+
+                U = []
+                # only add u if it has great possibility to be assigned to c
+                for u in vertexNotAdj[c]:
+                    if val[u,c] >= (1/VNum) + 0.001 :
+                        U.append(u)
+                #print(U)
+            
+                # for each node in U, solve for minimum vertex cut
+                
+                for u in U:
+                    cut_value, partition = nx.minimum_cut(GRAPH, str(c)+"@out", str(u)+"@in")
+                    # get minimum vertex cut
+                    cutset = set()
+                    reachable, non_reachable = partition
+                    for u1, nbrs in ((n, GRAPH[n]) for n in reachable):
+                        cutset.update((u1, v) for v in nbrs if v in non_reachable)
+
+                    S = []
+                    for v_temp in cutset:
+                        S.append(v_temp[0].split("@")[0])
+                    S = set(S)
+                    
+                    # Add cut if the sum of vertex weight is smaller than Xcu
+                    sumVertex = 0
+                    for v in S:
+                        sumVertex += val[v,c]
+                    
+                    if sumVertex < val[u,c] - 0.001:
+                        #print("hey")
+                        m._count = m._count + 1
+                        model.cbCut(gp.quicksum(model._vars[v,c]
+                                                for v in S) >= model._vars[u,c])
+                    
+            #model._vals = None
+            #print(centerList)
     
         
 m._vars = x
 m._vals = None
 m._count = 0
+m.Params.PreCrush = 1
 m.Params.LazyConstraints = 1
 m.optimize(vertexSeparatorCut)
 m.write("output.sol")
 
-CUTSol = {}
+CUTALLSol = {}
 for i in vertex:
     for j in vertex:
         if x[i,j].x >0.5:
-                CUTSol[i] = j
+                CUTALLSol[i] = j
 
 
-with open("CUTSol.pd","wb") as f:
-    pk.dump(CUTSol,f)
+with open("CUTALLSol.pd","wb") as f:
+    pk.dump(CUTALLSol,f)
 
 print("cuts : ",m._count)
